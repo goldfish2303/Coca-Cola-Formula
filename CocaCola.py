25b7c9d4a471d3387446a0a333724a9bc292afab
goldfish2303 <phantuanhao23.3@gmail.com>
2026-01-09 19:24:07 +0700

    Create CocaCola.py

---
+++ CocaCola.py
@@ -0,0 +1,16 @@
+import scale      #  +/- 0.01 g
+import micropipette      #  1000 µL
+import cylinder      #  50 mL
+import bottles      #  1 L
+import heatResistantBeakers
+
+mixtureA = "7X Flavor"
+mixtureB = "Water-based Mixture"
+
+def mix():
+    mix(mixtureA + mixtureB)
+    return CocaCola
+
+setHeatResistantBeaker(mixtureA + mixtureB)
+mix()
+print("1 liter o' CocaCola")
