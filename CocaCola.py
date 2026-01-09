4a9e8b1c5ded6be9e169ef317e79cb1a4ac44594
goldfish2303 <phantuanhao23.3@gmail.com>
2026-01-09 20:42:43 +0700

    Update CocaCola.py

--- CocaCola.py
+++ CocaCola.py
@@ -3,16 +3,29 @@ import micropipette      #  1000 µL
 import cylinder      #  50 mL
 import bottles      #  1 L
 import heatResistantBeakers
+import microwave
+import time
 
-mixtureA = "7X Flavor" * 1mL
-mixtureB = "Water-based Mixture" * 10mL
+mixtureA = 1mL # 7X Flavor
+mixtureB = 10mL # Water-based Mixture
 sugar = 104g
 carbonatedWater = H2O + CO2
+water = 100mL
+cool = roomTemperature
 
 def mix():
-    mix(mixtureA + mixtureB + sugar + carbonatedWater)
-    return CocaCola
+    mix(mixtureA + mixtureB + sugar + water)
+    container(heatResistantBeakers)
+    microwave()
+    if temp > 100
+        print("You f_cked up")
+        kill()
+    else
+        cover()
+        wait(temp = cool)
+return(syrup)
 
-setHeatResistantBeaker()
 mix()
+mix(syrup + carbonatedWater)
+
 print("1 liter o' CocaCola")
