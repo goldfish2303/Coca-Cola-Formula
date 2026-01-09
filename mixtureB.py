ad8f354dfdaf5dc16086afc44010d2472e18eba5
goldfish2303 <phantuanhao23.3@gmail.com>
2026-01-09 20:12:27 +0700

    Create mixtureB.py

---
+++ mixtureB.py
@@ -0,0 +1,17 @@
+import heater
+
+water1 = 200mL
+shankCaramelColor = 320mL
+glycerin = 175mL # or less
+vanillaExtract = 10mL
+phosphoricAcid = 85% * 45mL
+vinegar = 5% * 10mL
+wineTannins = 8g # or more
+caffeine = 9.65g
+water
+
+heater = ON
+
+def mix():
+  heat(water1)
+  mix(water1 + shankCaramelColor + glycerin + vanillaExtract + phosphoricAcid + vinegar + wineTannins + caffeine)
