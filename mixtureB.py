493065041aabe5529a63cd446ba07b1b98be2c3a
goldfish2303 <phantuanhao23.3@gmail.com>
2026-01-09 20:28:16 +0700

    Update mixtureB.py

--- mixtureB.py
+++ mixtureB.py
@@ -8,10 +8,28 @@ phosphoricAcid = 85% * 45mL
 vinegar = 5% * 10mL
 wineTannins = 8g # or more
 caffeine = 9.65g
-water
+water = H2 + O
 
 heater = ON
 
-def mix():
+def concentratedMixtureB():
   heat(water1)
   mix(water1 + shankCaramelColor + glycerin + vanillaExtract + phosphoricAcid + vinegar + wineTannins + caffeine)
+  return(concentratedMixtureB)
+
+concentratedMixtureB()
+
+mix(concentratedMixtureB + water)
+if (concentratedMixtureB + water) < 1L
+  continue()
+  mix()
+  mix()
+if (concentratedMixtureA + water) = 1L
+  return(mixtureB)
+  heater = OFF
+  print("There's your Mixture B. You will only need 10mL of it for 1L of Coke")
+if (concentratedMixtureB + water) > 1L
+  return(sh_t)
+  print("You have f_cked up")
+  kill()
+
